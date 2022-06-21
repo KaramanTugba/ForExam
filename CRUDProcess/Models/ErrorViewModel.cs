7b0cb20471348a4ffa0424402d8098893515e36f
Tuğba KARAMAN <97016474+KaramanTugba@users.noreply.github.com>
2022-06-21 18:32:45 +0300

    Add project files.

---
+++ CRUDProcess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDProcess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
