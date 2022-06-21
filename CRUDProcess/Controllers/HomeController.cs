6364ce1f4c30fc88f04521c7ee686188055f1988
Tuğba KARAMAN <97016474+KaramanTugba@users.noreply.github.com>
2022-06-21 23:01:48 +0300

    add :Create done.

--- CRUDProcess/Controllers/HomeController.cs
+++ CRUDProcess/Controllers/HomeController.cs
@@ -7,20 +7,40 @@ namespace CRUDProcess.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Context _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, Context context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public async Task<IActionResult>Create(Student student)
         {
-            return View();
+            await _context.AddAsync(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Student(int? Id)
+        {
+            Student student;
+            if (Id.HasValue)
+            {
+                student = _context.Student.Find(Id);
+            }
+            else
+            {
+                student = new Student();
+            }
+            
+            return View(student);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
