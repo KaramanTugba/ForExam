c0b4f102b14db4abcabe85d71d86fdd9850e533e
Tuğba KARAMAN <97016474+KaramanTugba@users.noreply.github.com>
2022-06-21 20:15:54 +0300

    add Student.cs and Context.cs

---
+++ CRUDProcess/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDProcess.Models
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options)
+        {
+
+        }
+        public DbSet<Student> Student { get; set; }
+    }
+}
